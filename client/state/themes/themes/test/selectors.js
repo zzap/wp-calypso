09434f41d79d056893d6896596411af458c75fed
Bernhard Reiter <ockham@raz.or.at>
2016-10-17 20:45:28 +0200

    Add lots of selector tests

--- client/state/themes/themes/test/selectors.js
+++ client/state/themes/themes/test/selectors.js
@@ -6,46 +6,317 @@ import { expect } from 'chai';
 /**
  * Internal dependencies
  */
-import { getThemeSignupUrl } from '../selectors';
+import {
+	getThemeDetailsUrl,
+	getThemeSupportUrl,
+	getThemeHelpUrl,
+	getThemePurchaseUrl,
+	getThemeCustomizeUrl,
+	getThemeSignupUrl
+} from '../selectors';
 
 describe( 'themes selectors', () => {
 	describe( '#getThemeDetailsUrl', () => {
+		it( 'given a theme and no site ID, should return the details URL', () => {
+			const detailsUrl = getThemeDetailsUrl(
+				{
+					sites: {
+						items: {
+							2916284: {
+								ID: 2916284,
+								URL: 'https://example.wordpress.com'
+							}
+						}
+					}
+				},
+				{
+					id: 'twentysixteen',
+					stylesheet: 'pub/twentysixteen'
+				}
+			);
+			expect( detailsUrl ).to.equal( '/theme/twentysixteen' );
+		} );
 
+		it( 'given a theme and wpcom site ID, should return the details URL', () => {
+			const detailsUrl = getThemeDetailsUrl(
+				{
+					sites: {
+						items: {
+							2916284: {
+								ID: 2916284,
+								URL: 'https://example.wordpress.com'
+							}
+						}
+					}
+				},
+				{
+					id: 'twentysixteen',
+					stylesheet: 'pub/twentysixteen'
+				},
+				2916284
+			);
+			expect( detailsUrl ).to.equal( '/theme/twentysixteen/example.wordpress.com' );
+		} );
+
+		it( 'given a theme and Jetpack site ID, should return the details URL', () => {
+			const detailsUrl = getThemeDetailsUrl(
+				{
+					sites: {
+						items: {
+							77203074: {
+								ID: 77203074,
+								URL: 'https://example.net',
+								jetpack: true,
+								options: {
+									admin_url: 'https://example.net/wp-admin/'
+								}
+							}
+						}
+					}
+				},
+				{
+					id: 'twentysixteen',
+					stylesheet: 'pub/twentysixteen'
+				},
+				77203074
+			);
+			expect( detailsUrl ).to.equal( 'https://example.net/wp-admin/themes.php?theme=twentysixteen' );
+		} );
 	} );
 
 	describe( '#getThemeSupportUrl', () => {
+		it( 'given a theme and no site ID, should return the support URL', () => {
+			const supportUrl = getThemeSupportUrl(
+				{
+					sites: {
+						items: {
+							2916284: {
+								ID: 2916284,
+								URL: 'https://example.wordpress.com'
+							}
+						}
+					}
+				},
+				{
+					id: 'mood',
+					stylesheet: 'premium/mood'
+				}
+			);
+			expect( supportUrl ).to.equal( '/theme/mood/setup' );
+		} );
 
+		it( 'given a theme and wpcom site ID, should return the support URL', () => {
+			const supportUrl = getThemeSupportUrl(
+				{
+					sites: {
+						items: {
+							2916284: {
+								ID: 2916284,
+								URL: 'https://example.wordpress.com'
+							}
+						}
+					}
+				},
+				{
+					id: 'mood',
+					stylesheet: 'premium/mood'
+				},
+				2916284
+			);
+			expect( supportUrl ).to.equal( '/theme/mood/setup/example.wordpress.com' );
+		} );
+
+		it( 'given a theme and Jetpack site ID, should return the support URL', () => {
+			const supportUrl = getThemeSupportUrl(
+				{
+					sites: {
+						items: {
+							77203074: {
+								ID: 77203074,
+								URL: 'https://example.net',
+								jetpack: true,
+								options: {
+									admin_url: 'https://example.net/wp-admin/'
+								}
+							}
+						}
+					}
+				},
+				{
+					id: 'twentysixteen',
+					stylesheet: 'pub/twentysixteen'
+				},
+				77203074
+			);
+			expect( supportUrl ).to.equal( '//wordpress.org/support/theme/twentysixteen' );
+		} );
 	} );
 
 	describe( '#getThemeHelpUrl', () => {
+		it( 'given a theme and no site ID, should return the help URL', () => {
+			const helpUrl = getThemeHelpUrl(
+				{
+					sites: {
+						items: {
+							2916284: {
+								ID: 2916284,
+								URL: 'https://example.wordpress.com'
+							}
+						}
+					}
+				},
+				{
+					id: 'mood',
+					stylesheet: 'premium/mood'
+				}
+			);
+			expect( helpUrl ).to.equal( '/theme/mood/support' );
+		} );
 
+		it( 'given a theme and a wpcom site ID, should return the correct help URL', () => {
+			const helpUrl = getThemeHelpUrl(
+				{
+					sites: {
+						items: {
+							2916284: {
+								ID: 2916284,
+								URL: 'https://example.wordpress.com'
+							}
+						}
+					}
+				},
+				{
+					id: 'mood',
+					stylesheet: 'premium/mood'
+				},
+				2916284
+			);
+			expect( helpUrl ).to.equal( '/theme/mood/support/example.wordpress.com' );
+		} );
+
+		it( 'given a theme and Jetpack site ID, should return the correct help URL', () => {
+			const helpUrl = getThemeHelpUrl(
+				{
+					sites: {
+						items: {
+							77203074: {
+								ID: 77203074,
+								URL: 'https://example.net',
+								jetpack: true,
+								options: {
+									admin_url: 'https://example.net/wp-admin/'
+								}
+							}
+						}
+					}
+				},
+				{
+					id: 'twentysixteen',
+					stylesheet: 'pub/twentysixteen'
+				},
+				77203074
+			);
+			expect( helpUrl ).to.equal( '//wordpress.org/support/theme/twentysixteen' );
+		} );
+	} );
+
+	describe( '#getPurchaseUrl', () => {
+		it( 'given a theme and a wpcom site ID, should return the purchase URL', () => {
+			const purchaseUrl = getThemePurchaseUrl(
+				{
+					sites: {
+						items: {
+							2916284: {
+								ID: 2916284,
+								URL: 'https://example.wordpress.com'
+							}
+						}
+					}
+				},
+				{
+					id: 'mood',
+					stylesheet: 'premium/mood'
+				},
+				2916284
+			);
+			expect( purchaseUrl ).to.equal( '/checkout/example.wordpress.com/theme:mood' );
+		} );
 	} );
 
 	describe( '#getThemeCustomizeUrl', () => {
+		it( 'given a theme and no site ID, should return the correct customize URL', () => {
+			const customizeUrl = getThemeCustomizeUrl(
+				{},
+				{
+					id: 'twentysixteen',
+					stylesheet: 'pub/twentysixteen'
+				}
+			);
+			expect( customizeUrl ).to.equal( '/customize/' );
+		} );
 
+		it( 'given a theme and wpcom site ID, should return the correct customize URL', () => {
+			const customizeUrl = getThemeCustomizeUrl(
+				{
+					sites: {
+						items: {
+							2916284: {
+								ID: 2916284,
+								URL: 'https://example.wordpress.com'
+							}
+						}
+					}
+				},
+				{
+					id: 'twentysixteen',
+					stylesheet: 'pub/twentysixteen'
+				},
+				2916284
+			);
+			expect( customizeUrl ).to.equal( '/customize/example.wordpress.com?theme=pub/twentysixteen' );
+		} );
+
+		// FIXME: In implementation, get rid of `window` dependency.
+		it.skip( 'given a theme and Jetpack site ID, should return the correct customize URL', () => {
+			const customizeUrl = getThemeCustomizeUrl(
+				{
+					sites: {
+						items: {
+							77203074: {
+								ID: 77203074,
+								URL: 'https://example.net',
+								jetpack: true
+							}
+						}
+					}
+				},
+				{
+					id: 'twentysixteen',
+					stylesheet: 'pub/twentysixteen'
+				},
+				77203074
+			);
+			expect( customizeUrl ).to.equal( '/customize/example.wordpress.com?theme=pub/twentysixteen' );
+		} );
 	} );
 
 	describe( '#getThemeSignupUrl', () => {
-		context( 'with a free theme', () => {
-			it( 'should return the correct signup URL', () => {
-				const signupUrl = getThemeSignupUrl( {}, {
-					id: 'twentysixteen',
-					stylesheet: 'pub/twentysixteen'
-				} );
-
-				expect( signupUrl ).to.equal( '/start/with-theme?ref=calypshowcase&theme=twentysixteen' );
+		it( 'given a free theme, should return the correct signup URL', () => {
+			const signupUrl = getThemeSignupUrl( {}, {
+				id: 'twentysixteen',
+				stylesheet: 'pub/twentysixteen'
 			} );
+
+			expect( signupUrl ).to.equal( '/start/with-theme?ref=calypshowcase&theme=twentysixteen' );
 		} );
 
-		context( 'with a premium theme', () => {
-			it( 'should return the correct signup URL', () => {
-				const signupUrl = getThemeSignupUrl( {}, {
-					id: 'mood',
-					stylesheet: 'premium/mood'
-				} );
-
-				expect( signupUrl ).to.equal( '/start/with-theme?ref=calypshowcase&theme=mood&premium=true' );
+		it( 'given a premium theme, should return the correct signup URL', () => {
+			const signupUrl = getThemeSignupUrl( {}, {
+				id: 'mood',
+				stylesheet: 'premium/mood'
 			} );
+
+			expect( signupUrl ).to.equal( '/start/with-theme?ref=calypshowcase&theme=mood&premium=true' );
 		} );
 	} );
 } );
